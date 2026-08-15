2b7a4255cf74d4a5e97ffbbb12e3f649d151ef5e
Akshay6006 <akshaysingh09oct@gmail.com>
2026-08-15 15:44:10 +0530

    Build EV ChargeHub platform and landing page

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPjZ3lu2dvuDgKcvpxLaarMnzj7n1si7Y",
-  authDomain: "ev-recharge-bunk-dbd05.firebaseapp.com",
-  projectId: "ev-recharge-bunk-dbd05",
-  storageBucket: "ev-recharge-bunk-dbd05.firebasestorage.app",
-  messagingSenderId: "376503343817",
-  appId: "1:376503343817:web:564092ebe808ad8bbe4b18"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
